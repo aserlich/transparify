bab3c5154c4a11fd2a62cb0678e851ff28d9818f
Aaron Erlich <aaron.erlich@gmail.com>
2014-05-22 15:01:59 +0400

    version which takes in to consideration cases where there is only one rater and coverts -1s to 0s. It also spits out a complete set of files for future use. Another script should be written later to deal with these files, which have been added to.

--- format_transparify.r
+++ format_transparify.r
@@ -1,16 +1,23 @@
 #last updated Friday March 21, 2014
-#This file reads in an Excel file and transforms it into the proper format to do Krippendorff's analsys
+#This file reads in an Excel file and transforms it into the proper format to do Krippendorff's analysis
 #this program is designed to work with two directories one with the data files a directory call krip_data
 #and the .r files in a directory called krip_code
-
+rm(list=ls())
 library(gdata) #not necessary with not .xls files, which we should have
 library(irr)
 library(reshape2)
+library(plyr)
 
-#setwd("/Volumes/Optibay-1TB/Consulting/Transparify/krip_code")
-mypath <- getwd()
+#The two raters to be removed were RS and ZT. 
 
-files <- dir("../krip_data", pattern = "*.csv")
+
+dpath <- "/Volumes/Optibay-1TB/Consulting/Transparify/krip_code"
+
+setwd(dpath)
+
+file <- "transparify_20140324_2.csv"
+
+files <- dir("../krip_data", pattern = file)
 
 sprintf("There are %d files in the data directory", length(files)) 
 
@@ -33,23 +40,69 @@ unqRaters <- sort(levels(try1$Rater))
 cat("There are the following unique raters\n")
 cat(unqRaters)
 
-dataWide <- dcast(try1,Rater ~ TTName, value.var="Score", fill =NA, fun.aggregate = length)
-write.csv(file="../krip_data/transpartifyWide.csv", dataWide)
-write.csv(file="../krip_data/transpartifyLong.csv", try1)
-testDups <- aggregate(try1, by=list(try1$Rater, try1$TTName), length)
-
 #check for duplicates -- this should quit the program, if there are any duplicates, since
 #there should be no calculations made on data with errors in it
-Dups <- testDups[testDups$Continent>1, 1:3]
-names(Dups)[1:3] <- c("Rater", "Org", "NumDups")
-if(nrow(Dups) > 0){
-    sprintf("You have %d records, where you have duplicate ratings from the same rater", nrow(Dups))
-    print(Dups[1:3])
-}
+## testDups <- aggregate(try1[!try1$Rater %in% c("adj")], by=list(try1$Rater, try1$TTName), length)
+## Dups <- testDups[testDups$Rater>1, 1:3]
+## names(Dups)[1:3] <- c("Rater", "Org", "NumDups")
+## if(nrow(Dups) > 0){
+##     sprintf("You have %d records, where you have duplicate ratings from the same rater", nrow(Dups))
+##     print(Dups[1:3])
+## }
 #for the time being arbitrarily remove one duplicate
 try2 <- try1[!duplicated(try1[, c("Rater", "TTName")]), ]
-dataWide <- dcast(try2,Rater ~ TTName, value.var="Score", fill =NA)
-write.csv(file="../krip_data/transpartifyWide.csv", dataWide)
-krippData <- as.matrix(dataWide[dataWide$Rater != "adj", 2:ncol(dataWide) ])
-tpfyKrippAlpha <- kripp.alpha(x=krippData, method ="ordinal") 
-write.csv(file=file.path("../krip_data/", paste0("transpartifyRating-", Sys.Date(), ".csv")))
+try2 <- try2[order(try2$TTName), ]
+allOrgs <-  unique(try2$TTName)
+
+#only run once and never again
+if(!("uuid" %in% names(try2))) {
+    uniqueIDs <- replicate(length(allOrgs), system("uuidgen", intern=TRUE))
+    orgUUIDs <- as.data.frame(cbind(TTName=allOrgs, uuid = uniqueIDs), stringsAsFactors=FALSE)
+    write.csv(file=paste0("../krip_data/","uniqueTTCodes", Sys.Date(), ".csv"), orgUUIDs)
+    try2 <- join(try2, orgUUIDs, by="TTName", type="left")
+    try2[try2$Score==-1,c("Score", "adjScore")] <- 0
+    dataWide <- dcast(try2,Rater ~ TTName, value.var="Score", fill =NA)
+}
+
+orgData <- dcast(try2,uuid+TTName~Rater, value.var="Score", fill =NA)
+#dataWide <- dcast(try1, Rater ~ TTName, value.var="Score", fill=NaN, fun.aggregate = length)
+#dataWide[is.na(dataWide)] <- NA
+write.csv(file=paste0("../krip_data/","cleanedTransparifyDataWide", Sys.Date(), ".csv"), dataWide)
+write.csv(file=paste0("../krip_data/","cleanedTransparifyData", Sys.Date(), ".csv"), try2)
+write.csv(file=paste0("../krip_data/","uniqueTransparifyTTs", Sys.Date(), ".csv"), orgData)
+
+##This can be used for further iterations of the data which come in the this form as they should
+##this should be separated into a separate R script
+#get a unique data sheet with the raters
+raters <- c( "AE", "AP", "AWT", "HG", "JL", "KE", "MG", "MOK", "MS", "NM", "NP", "RB", "RS",  "RS",  "TA",  "TB",  "TBD", "UB",  "VB",  "ZT")
+
+#remove all with only one rating
+orgData$totalnonAdjRatings <- apply(orgData[, raters], 1, function(x) length(na.omit(x)))
+orgData$totalRatings <- apply(orgData[, c("adj",raters)], 1, function(x) length(na.omit(x)))
+
+oneRateOrgs <- orgData[orgData$totalnonAdjRatings <2,"TTName"]
+rateableTTs <- orgData$TTName[!(orgData$TTName %in% oneRateOrgs)]
+
+##Rate orgs
+krippDataAll <- as.matrix(dataWide[!(dataWide$Rater  %in% c("adj")),  rateableTTs])
+krippDataGood <- as.matrix(dataWide[!(dataWide$Rater  %in% c("adj", "VB", "ZT")), rateableTTs ])
+tpfyKrippAlphaAll <- kripp.alpha(x=krippDataAll, method ="ordinal")
+tpfyKrippAlphaGood <- kripp.alpha(x=krippDataGood, method ="ordinal")
+
+raterStats <- ddply(try2[!try2$Rater %in% c("adj"), ], .(Rater) ,c(function(x) sum(x$Score==x$adjScore, na.rm=TRUE), function(x) sum(x$Score!=x$adjScore, na.rm=TRUE)))
+names(raterStats) <- c("Rater", "numRight", "numWrong")
+
+raterStats$Total <- raterStats$numRight+raterStats$numWrong
+raterStats$pctCorrect <- raterStats$numRight/raterStats$Total
+
+sum(raterStats$numRight)/sum(raterStats$Total)
+
+write.csv(file=paste0("../krip_data/","ratersStats", Sys.Date(), ".csv"), raterStats)
+
+
+#write.csv(file=file.path("../krip_data/", paste0("transpartifyRating-", Sys.Date(), ".csv")))
+
+mean(raterStats$pctCorrect)
+sum(raterStats$pctCorrect*raterStats$Total)/sum(raterStats$Total)
+
+#write.csv(file=file.path("../krip_data/", paste0("onlyOneRater-", Sys.Date(), ".csv")), orgData[orgData$totalnonAdjRatings <2,])
